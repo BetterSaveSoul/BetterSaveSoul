94e2458d764f33d3c80b2bad20bf5b4516df5ae1
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-06-12 14:29:29 +0300

    feat: add geoloaction table sql script #4

---
+++ src/Database/BssSQLServerDb/dbo/Tables/GeoLocation.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[GeoLocation]
+(
+	LocationID INT FOREIGN KEY REFERENCES Location(LocationID),
+    Latitude DECIMAL(9,6),
+    Longitude DECIMAL(9,6),
+    PRIMARY KEY (LocationID)
+)
