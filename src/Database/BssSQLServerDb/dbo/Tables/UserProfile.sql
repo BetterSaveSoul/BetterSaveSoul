6ab4e95cf056059f664ab131c0f2fe5787b0ec35
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-06-12 14:32:37 +0300

    feat: add user profile table sql script #4

---
+++ src/Database/BssSQLServerDb/dbo/Tables/UserProfile.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[UserProfile]
+(
+    UserId INT PRIMARY KEY,
+    FirstName NVARCHAR(100) NOT NULL,
+    LastName NVARCHAR(100) NOT NULL,
+    DateOfBirth DATE NOT NULL,
+    LocationID INT FOREIGN KEY REFERENCES Location(LocationID) NOT NULL,
+    ProfilePicture NVARCHAR(MAX),
+    Bio NVARCHAR(255),
+    DateJoined DATETIME NOT NULL,
+    IsActive BIT NOT NULL
+)
