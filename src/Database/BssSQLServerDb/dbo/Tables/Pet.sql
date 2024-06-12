ac0b4482bd96bd89a0da65f7a122a8b1e9da7801
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-06-12 14:40:04 +0300

    feat: add pet table sql script #4

---
+++ src/Database/BssSQLServerDb/dbo/Tables/Pet.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Pet]
+(
+	PetID BIGINT PRIMARY KEY,
+    Name NVARCHAR(100),
+    SpeciesID INT FOREIGN KEY REFERENCES Species(SpeciesID) NOT NULL,
+    Breed INT FOREIGN KEY REFERENCES Breed(BreedID),
+    DateOfBirth DATE NOT NULL,
+    Gender SMALLINT FOREIGN KEY REFERENCES Gender(GenderID),
+    Color NVARCHAR(50),
+    Size NVARCHAR(10),
+    HealthStatus  SMALLINT FOREIGN KEY REFERENCES PetHealthStatus(PetHealthStatusID) NOT NULL,
+    Vaccinated BIT NOT NULL,
+    NeuteredStatus BIT NOT NULL,
+    Description NVARCHAR(255),
+    OwnerProfileID INT FOREIGN KEY REFERENCES UserProfile(UserID),
+)
